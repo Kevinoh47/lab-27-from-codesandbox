ab53cd81ebfb9eb062d5cc7dda94ecbf1fd26d15
Kevinoh47 <kohok47@gmail.com>
2018-12-17 12:21:01 -0800

    counter and counter tests

--- src/__tests__/components/counter/counter.test.js
+++ src/__tests__/components/counter/counter.test.js
@@ -1,3 +1,29 @@
-import React from "react";
 import renderer from "react-test-renderer";
-import Counter from "components/counter/counter.js";
+import Counter from "components/counter/counter.js"; //hard coded to avoid .env issue
+
+describe("counter", () => {
+  it("is alive at application start", () => {
+    let app = shallow(<Counter />);
+    expect(app.find("section").exists()).toBeTruthy();
+  });
+
+  it("changes state by decrementing the count when the down link is clicked", () => {
+    let app = mount(<Counter />);
+    let downClick = app.find("a.downClicker");
+    downClick.simulate("click");
+    expect(app.state("count")).toEqual(-1);
+  });
+
+  it("changes state by incrementing the count when the up link is clicked", () => {
+    let app = mount(<Counter />);
+    let downClick = app.find("a.upClicker");
+    downClick.simulate("click");
+    expect(app.state("count")).toEqual(1);
+  });
+
+  // this test cannot run on codesandbox.io
+  it("renders, and matches snapshot", () => {
+    const tree = renderer.create(<Counter />);
+    expect(tree).toMatchSnapshot();
+  });
+});
